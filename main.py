347b686bd3d6292032cbe0a49d8bc7cd67903084
Rob Wilds <robwilds@me.com>
2025-07-16 18:03:17 -0400

    added audit details tool

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from server import mcp
 # Import tools so they get registered via decorators
 import tools.csv_tools
 import tools.parquet_tools
-import tools.getCommentsForNode
+import tools.alfrescoAPI
 
 # Entry point to run the server
 if __name__ == "__main__":
     
     mcp.run()
-    print('main server running');
+    #print('main server running');
---
+++ tools/alfrescoAPI.py
@@ -0,0 +1,134 @@
+import pandas as pd, json
+import os
+from dotenv import load_dotenv
+import requests,json
+from server import mcp
+from datetime import datetime,timezone
+from zoneinfo import ZoneInfo
+from tools.queryAlfAPI import runQuery
+
+# Load environment variables from the .env file
+load_dotenv()
+
+BASE_URL= os.getenv("BASE_URL")
+auth = os.getenv("auth")
+user=os.getenv("user")
+passwd=os.getenv("pass")
+
+nodeID = []
+appEntryID = []
+appEntryDetails = []
+appEntryDate = []
+appEntryUser = []
+
+
+cols = {0: 'nodeID',1:'auditEntryID',2:'entryDate',3:'details',4:'user'}
+
+@mcp.tool()
+def getComments(nodeid: str) -> str:
+    """
+    Get comments for a node in Alfresco.
+    Args:
+        nodeid: The ID of the node to retrieve comments for.
+    Returns:
+        A JSON string containing the comments for the specified node.
+    """
+
+    url = os.getenv("BASE_URL") + "/alfresco/api/-default-/public/alfresco/versions/1/nodes/"+nodeid+"/comments"
+    print('this is the url: '+url); 
+    temp4 = requests.get(url,auth = (os.getenv("user"), os.getenv("pass"))).text
+    print(temp4)
+    return temp4
+
+@mcp.tool()
+def getAuditApps() -> str:
+    """
+    Get audit apps enabled for Alfresco.
+    Args:
+        none
+    Returns:
+        A JSON string containing the audit apps with enabled status.
+    """
+
+    url = os.getenv("BASE_URL") + "/alfresco/api/-default-/public/alfresco/versions/1/audit-applications"
+    print('this is the url: '+url); 
+    temp4 = requests.get(url,auth = (os.getenv("user"), os.getenv("pass"))).text
+    print(temp4)
+    return temp4
+
+def pullAuditEntryForNode(nodeid):
+
+    auditEntryforNodeQuery = BASE_URL + '/alfresco/api/-default-/public/alfresco/versions/1/nodes/'+nodeid+'/audit-entries'
+
+    print ('query url is: ' + auditEntryforNodeQuery + ' with userpass: '+ user+passwd) #debug
+    data=runQuery('get',auditEntryforNodeQuery,'',user,passwd)
+    print ("data from nodequery returned is: " + json.dumps(data)) #debug
+    return data
+
+def pullAuditEntryDetailsForNode(auditentryid):
+
+    auditEntryDetailsForNodeQuery = BASE_URL + '/alfresco/api/-default-/public/alfresco/versions/1/audit-applications/alfresco-access/audit-entries/'+str(auditentryid)+''
+
+    data=runQuery('get',auditEntryDetailsForNodeQuery,'',user,passwd)
+
+    print('data from entry detailsfornode: '+ json.dumps(data))
+
+    #See if the actual action data can be returned
+    actionDetailsForNode = data['entry']['values']['/alfresco-access/transaction/action'] #
+    #actionDetailsForNode = data['entry']['values']['/alfresco-access/transaction/sub-actions']
+    actionUserForNode = data['entry']['values']['/alfresco-access/transaction/user']
+
+    return [actionDetailsForNode,actionUserForNode]
+
+def dateProcessor(date):
+
+    date_format = "%Y-%m-%dT%H:%M:%S.%f+0000"
+    local_tz = ZoneInfo("America/New_York")
+    
+    newDate = datetime.strptime(date,date_format)
+    #print ('inside dateprocessor: '+str(newDate)) #debug
+
+    print('local time zone '+ str(newDate.astimezone(local_tz)))
+
+    return str(newDate.astimezone(local_tz))
+
+@mcp.tool()
+def auditForNode(nodeid):
+    """
+    Get audit entries for a specific node in Alfresco.
+    Args:
+        nodeid: The ID of the node to retrieve audit entries for.
+    Returns:
+        A DataFrame containing the audit entries for the specified node.
+    """
+    #now clear the temp arrays now!
+    nodeID = []
+    appEntryID = []
+    appEntryDate = []
+    appEntryDetails = []
+    appEntryUser = []
+    auditentryfornodeDF = pd.DataFrame(None)
+
+    #print("node id is: "+nodeid) #debugging
+
+    for entry in pullAuditEntryForNode(nodeid)['list']['entries']:
+        #print(nodeid + '-' + str(entry['entry']['id'])) #debug
+
+        #set the entry details and user now so there's one call
+        entryDetails = pullAuditEntryDetailsForNode(entry['entry']['id'])[0]
+        entryUser = pullAuditEntryDetailsForNode(entry['entry']['id'])[1]
+
+        nodeID.append(nodeid) #this will be the same nodeid for each audit entry id
+        appEntryID.append(entry['entry']['id'])
+        appEntryDate.append(dateProcessor(entry['entry']['createdAt']))
+        appEntryDetails.append(entryDetails) #this is coming from the pullauditdetailsentryfornode
+        appEntryUser.append(entryUser) #this is coming from the pullauditdetailsentryfornode
+    
+    #print ("size of user array is: " + str(len(appEntryUser))) #debugging
+    auditentryfornodeDF = pd.DataFrame([nodeID,appEntryID,appEntryDate,appEntryDetails,appEntryUser]).T
+    auditentryfornodeDF.rename(columns=cols,inplace=True)
+
+    print (auditentryfornodeDF)
+    auditentryfornodeDF.to_excel('auditentryfornode.xlsx')
+
+    return auditentryfornodeDF
