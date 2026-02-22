b23a532f3904e4135045de51dd81b58d56d58512
Rob Wilds <robwilds@me.com>
2026-02-22 06:19:33 -0500

    updated to fastmcp v3 and added async to main

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import asyncio
 from server import mcp
 
 # Import tools so they get registered via decorators
 import tools.alfrescoAPI
 #import tools.tylerConn
 
-mcp.run(transport="http")
+async def main():
+	await mcp.run_async(transport="streamable-http", stateless_http=True)
+
 # Entry point to run the server
+if __name__ == "__main__":
+	asyncio.run(main())
--- server.py
+++ server.py
@@ -4,4 +4,4 @@ from fastmcp import FastMCP
 import asyncio
 # This is the shared MCP server instance
 
-mcp = FastMCP("Alfresco MCP Server by RobW",host="0.0.0.0",log_level="DEBUG",stateless_http=True,)
+mcp = FastMCP("Alfresco MCP Server by RobW")
